5d671beac77d2c957874b285a62940d9b3929085
Sabir Süleymanlı <suleymanlisabir3@gmail.com>
2023-05-26 00:03:49 +0400

    + create openmenu() and closemenu()

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = Document.getElementById("sidemenu");
+
+
+function openmenu() {
+    sidemenu.style.right = "0";    
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";  
+}
